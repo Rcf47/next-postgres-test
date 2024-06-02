e7639325c792c24c7b9729bfd66f8fa212e61f94
Vadim Cherepanov <xenos26@gmail.com>
2024-06-02 22:17:34 +0700

    refactor: change export from functions.ts

--- lib/prisma/functions.ts
+++ lib/prisma/functions.ts
@@ -1,8 +1,6 @@
 import prisma from "@/lib/prisma/prisma";
 
-async function getUsers() {
+export async function getUsers() {
   const users = await prisma.users.findMany();
   return users;
 }
-
-export default getUsers;
