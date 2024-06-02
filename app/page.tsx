c50daef79aee5c4ce569ec194d19bbefe750628b
Vadim Cherepanov <xenos26@gmail.com>
2024-06-03 00:08:52 +0700

    feat: add link button on main page for add data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Card from "@/components/shared/Card/Card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <div>
         <Card />
+        <Button asChild className="mt-2">
+          <Link href="/add-data">Add data</Link>
+        </Button>
       </div>
     </>
   );
