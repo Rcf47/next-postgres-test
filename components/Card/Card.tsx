404e29f8b052bd5dfaa848e9bd3ddcb5556dc6a6
Vadim Cherepanov <xenos26@gmail.com>
2024-06-02 19:17:37 +0700

    feat: start working with card component

---
+++ components/Card/Card.tsx
@@ -0,0 +1,19 @@
+import getUsers from "@/lib/prisma/functions";
+
+const Card = async () => {
+  const users = await getUsers();
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <div key={user.id} className="flex gap-2 hover:flex-col ">
+            <div className="w-16 bg-sky-50">{user.name}</div>
+            <div className="w-[450px] bg-red-300">{user.surname}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Card;
