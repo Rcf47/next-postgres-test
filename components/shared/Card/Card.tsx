0fc8cbf53d6bd4f95e0be3be667721604c939c14
Vadim Cherepanov <xenos26@gmail.com>
2024-06-03 21:36:22 +0700

    feat: add delete data on main page

--- components/shared/Card/Card.tsx
+++ components/shared/Card/Card.tsx
@@ -1,4 +1,7 @@
+import DeleteData from "@/components/shared/DeleteData/DeleteData";
 import { getUsers } from "@/lib/prisma/functions";
+import Image from "next/image";
+import Link from "next/link";
 
 const Card = async () => {
   const users = await getUsers();
@@ -6,9 +9,10 @@ const Card = async () => {
     <div className="flex gap-4 flex-wrap">
       {users.map((user) => {
         return (
-          <div key={user.id} className="flex gap-2 border p-2 w-48">
+          <div key={user.id} className="flex gap-2 border p-2 items-start">
             <div className="w-16 bg-sky-50">{user.name}</div>
-            <div className="w-[450px] bg-red-300">{user.surname}</div>
+            <div className="w-[250px] bg-red-300">{user.surname}</div>
+            <DeleteData userId={user.id} />
           </div>
         );
       })}
