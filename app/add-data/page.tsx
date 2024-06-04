9a941ab6cb295225ebb242f482d8c76e98bd4a46
Vadim Cherepanov <xenos26@gmail.com>
2024-06-04 22:23:29 +0700

    feat: add title on add data page

--- app/add-data/page.tsx
+++ app/add-data/page.tsx
@@ -9,6 +9,8 @@ function AddData() {
         <Button asChild className="shadow-lg shadow-white">
           <Link href="/">Home</Link>
         </Button>
+
+        <h1 className="text-center text-4xl text-white">Add data</h1>
       </div>
     </main>
   );
