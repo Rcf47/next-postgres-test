2c5d2d5a8df70f9e9412205b98ebcebe06a4c206
Vadim Cherepanov <xenos26@gmail.com>
2024-06-05 20:16:09 +0700

    feat: add props for data form

--- app/edit-data/[userId]/page.tsx
+++ app/edit-data/[userId]/page.tsx
@@ -1,13 +1,12 @@
 import DataForm from "@/components/shared/DataForm/DataForm";
 import HomeButton from "@/components/shared/HomeButton/HomeButton";
-import { Data } from "@/types";
 
-function EditData() {
+function EditData({ params }: { params: { userId: string } }) {
   return (
     <>
       <HomeButton />
       <h1 className="text-center text-4xl text-white">Edit data</h1>
-      <DataForm type="edit" />
+      <DataForm type="edit" userId={params.userId} />
     </>
   );
 }
